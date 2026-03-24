de4fa45c23c2ccb889a276d406e60277e6a48492
AnkitaSajiv <ankitasajiv7@gmail.com>
2026-03-24 16:23:17 +0530

    UC7: Deque-based palindrome check

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "madam";
+
+        // Create Deque
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < word.length(); i++) {
+            deque.add(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
