7f0049f8f301047417d45e5e185ee45a66a9ea5c
AnkitaSajiv <ankitasajiv7@gmail.com>
2026-03-24 15:59:09 +0530

    UC2: Hardcoded palindrome

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+public class UseCase2PalindromeCheckerApp {
+
+    // Method to check palindrome
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Check and print result
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
